5cfd486db79cecb28e5443f9ee203b95dbd1a478
TrickyLeifa <26681464+TrickyLeifa@users.noreply.github.com>
2026-09-17 22:24:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,36 @@
 
 #include <cstdlib>
 
-kenji::Server *server;
-
-void cleanup()
-{
-  server->deleteLater();
-}
-
 int main(int argc, char *argv[])
 {
-  kenji::ConfigManager config;
-
-  QCoreApplication app(argc, argv);
-  QCoreApplication::setApplicationName("kenji");
-  QCoreApplication::setApplicationVersion("jackfruit (1.9)");
-  std::atexit(cleanup);
-
-  theory::PacketFactory packet_factory;
-  theory::registerPackets(packet_factory);
-
-  // Verify server configuration is sound.
-  if (!kenji::ConfigManager::verifyServerConfig())
+  int code = 0;
   {
-    zCritical(kenji::log::main) << "config.ini is invalid!";
-    zCritical(kenji::log::main) << "Exiting server due to configuration issue.";
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    server = new kenji::Server(kenji::ConfigManager::serverPort(), packet_factory, &app);
-    if (!server->start())
+    kenji::ConfigManager config;
+
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("kenji");
+    QCoreApplication::setApplicationVersion("jackfruit (1.9)");
+
+    theory::PacketFactory packet_factory;
+    theory::registerPackets(packet_factory);
+
+    // Verify server configuration is sound.
+    if (!kenji::ConfigManager::verifyServerConfig())
+    {
+      zCritical(kenji::log::main) << "config.ini is invalid!";
+      zCritical(kenji::log::main) << "Exiting server due to configuration issue.";
+      return EXIT_FAILURE;
+    }
+
+    kenji::Server server{kenji::ConfigManager::serverPort(), packet_factory};
+    if (!server.start())
     {
       zCritical(kenji::log::main) << "server failed to start";
       return EXIT_FAILURE;
     }
+
+    code = app.exec();
   }
 
-  return app.exec();
+  return code;
 }
